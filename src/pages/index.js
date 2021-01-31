14dadde89ec896b2eb96a012185b2cd6df32c46f
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-01 03:55:25 +0500

    Import products and prices via source plugin
    	1:- "npm install --save gatsby-source-stripe"
    	2:- update gatsby-config.js
    	3:- create .env.development
    	4:- create productlist.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,26 @@
+// Imports
+// React Imports
 import React from "react";
+// Stripe js Imports
 import { loadStripe } from "@stripe/stripe-js";
 
+// Configure stripe store
 const stripePromise = loadStripe("pk_test_51IFmaEEKbjR9lGIBPgyCVzUgtqE5CmDfl85dn8CzauY9GfbqwMfSpvORr9MxmDEP5PmWSuQoa6qR99czaRvwESh500huXBL1AJ")
 
-export default function Home() {
+// Page Exports
+export default function Home({location}) {
  
+  // On Checkout Function
   const redirectToCheckout = async()=>{
+    // load stripe from configration
     const stripe = await stripePromise;
+    // Function to be execyted while checkout
     const result = await stripe.redirectToCheckout({
       mode: "payment",
-      lineItems: [{ price: "price_1IFmpxEKbjR9lGIBEk9T5OKD", quantity: 1 }],
-      successUrl: `http://localhost:8888/payment-success/`,
-      cancelUrl: `http://localhost:8888/payment-error`,
+      lineItems: [{ price: "price_1IFmpxEKbjR9lGIBEk9T5OKD", quantity: 1 },
+                  { price: "price_1IFmnREKbjR9lGIBfLo3GnbE", quantity: 2 }],
+      successUrl: `${location.origin}/payment-success/`,
+      cancelUrl: `${location.origin}/payment-error`,
     })
 
   }
