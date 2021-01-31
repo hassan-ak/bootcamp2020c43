e56d272e04ac6fa844b6b9fe6f456ef4443a92be
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-01 02:12:44 +0500

    Integrate Stripe in the app
    	1:- gatsby new bootcamp2020c43 https://github.com/gatsbyjs/gatsby-starter-hello-world
    	2:- create an empty repo on github.com
    	3:- link repo with remote "git remote add origin https://github.com/hassan-ak/bootcamp2020c43.git"
    	4:- "netlify dev"
    	5:- "npm install --save @stripe/stripe-js"
    	6:- Signup on stripe.com
    	7:- From settings and checkout settings enable checkout client-only integration
    	8:- create products for testing
    	9:- "npm install --save dotenv"
    	10:- update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
-import React from "react"
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe("pk_test_51IFmaEEKbjR9lGIBPgyCVzUgtqE5CmDfl85dn8CzauY9GfbqwMfSpvORr9MxmDEP5PmWSuQoa6qR99czaRvwESh500huXBL1AJ")
 
 export default function Home() {
-  return <div>Hello world!</div>
+ 
+  const redirectToCheckout = async()=>{
+    const stripe = await stripePromise;
+    const result = await stripe.redirectToCheckout({
+      mode: "payment",
+      lineItems: [{ price: "price_1IFmpxEKbjR9lGIBEk9T5OKD", quantity: 1 }],
+      successUrl: `http://localhost:8888/payment-success/`,
+      cancelUrl: `http://localhost:8888/payment-error`,
+    })
+
+  }
+
+  return (
+    <div>
+      <div>Hello world!</div>
+      <button onClick={redirectToCheckout}>Checkout</button>
+    </div>
+    
+  )
 }
