0e21126b7d8157a6b1d1c082698b635b35282fc5
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-01 15:41:23 +0500

    checkout with functions/server (product from server)
    1:- update checkout.js

--- functions/checkout/checkout.js
+++ functions/checkout/checkout.js
@@ -4,13 +4,23 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 exports.handler = async event => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    line_items: [{price:"price_1IFmnREKbjR9lGIBfLo3GnbE",quantity:1},],
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: 'T-shirt',
+          images: ["https://ae01.alicdn.com/kf/HTB1eB_4XtjvK1RjSspiq6AEqXXaU/Fengdong-fashion-school-backpack-for-girls-school-bags-female-bagpack-children-backpacks-kids-cute-USB-bag.jpg"]
+        },
+        unit_amount: 2000,
+      },
+      quantity: 1,
+    }],
     mode: 'payment',
     success_url: 'http://localhost:8888/payment-success/',
     cancel_url: 'http://localhost:8888/payment-error/',
   });
   try {
-
+    
     return {
       statusCode: 200,
       body: JSON.stringify({ id: session.id }),
